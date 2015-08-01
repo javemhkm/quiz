7d5f5c752b0fe1dde46862b56bca320b4e1ab5c5
Javier Echevarria <javemhkm@gmail.com>
2015-08-01 23:44:19 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,11 +1,29 @@
 var path = require('path');
 
+// Postgres DATABASE_URL= postgres://user:passwd@host:port/database
+// SQLite DATABASE_URL= sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require ('sequelize');
 
-// Usar DB SQLite
-var sequelize = new Sequelize(null, null, null,
-	{dialect: "sqlite", storage: "quiz.sqlite"}
+// Usar DB SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{ dialect: protocol,
+	  protocol: protocol,
+	  port: port,
+	  host: host,
+	  storage: storage, // solo SQLite (.env)
+	  omitNull: true    // solo Postgres
+	}
 );
 
 // Importar la definicion de la tabla Quiz en quiz.js
@@ -16,7 +34,7 @@ exports.Quiz = Quiz; //exportar definicion de tabla Quiz
 sequelize.sync().success(function() {
 	// success(..) ejecuta el mantejador una vez creada la tabla
 	Quiz.count().success(function(count) {
-		if(count === 0) { // la tabla se inicializa solo si está vacía
+		if(count === 0) { // la tabla se inicializa solo si esta vacia
 			Quiz.create({ 	pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
 			})
