93e6b3e6f5b9a5d10b20f6fd852594691b8eaefd
Javier Echevarria <javemhkm@gmail.com>
2015-08-03 10:25:02 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,6 +55,29 @@ exports.answer = function(req, res) {
 	res.render ('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+	res.render ('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate().then(function(err){
+		if(err) {
+			res.render('/quizes/edit', {quiz: quiz, errors: err.errors});
+		} else {
+			//save: guarda campos pregunta y respuesta en DB
+			req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			})	// res.redirect: Redirección HTTP a lista de preguntas (URL relativo)
+		}
+	});
+};
+
 // GET /author
 exports.author = function(req, res) {
 	res.render('author', {autor: 'Javier Echevarría Martínez', errors: []});
