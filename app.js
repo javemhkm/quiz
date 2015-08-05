ddc93658a463e968906fd119eaa441b25c2d8d57
Javier Echevarria <javemhkm@gmail.com>
2015-08-05 10:22:47 +0200

    Buscador de preguntas

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({}));
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
