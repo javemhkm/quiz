267518a9344b3e132d3fb6e181f3df90b5f1cea5
Javier Echevarria <javemhkm@gmail.com>
2015-08-10 12:36:39 +0200

    Moderacion de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,11 +1,27 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+	models.Comment.find({
+		where: {
+			id: Number(commentId)
+        }
+	}).then(function(comment) {
+		if (comment) {
+			req.comment = comment;
+			next();
+		} else {
+			next(new Error('No existe commentId=' + commentId))
+		}
+    }).catch(function(error){next(error)});
+};
+
 // GET /quizes/:quizId(\\d+)/comments/new
 exports.new = function(req, res) {
 	res.render('comments/new', {quizId: req.params.quizId, errors: []});
 };
 
-// POST /quizes/:quizId(\\d+)/comment
+// POST /quizes/:quizId(\\d+)/comments
 exports.create = function(req, res) {
 	var comment = models.Comment.build( {
 		texto: req.body.comment.texto,
@@ -21,4 +37,13 @@ exports.create = function(req, res) {
 			})	// res.redirect: Redirección HTTP a la preguntas
 		}
 	}).catch(function(error){next(error)});
+};
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+	req.comment.publicado = true;
+
+	req.comment.save({fields: ["publicado"]})
+		.then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
+		.catch(function(error){next(error)});
 };
